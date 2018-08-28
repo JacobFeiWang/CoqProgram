0e19a7e32fefcc6bddd65c83be050ea8470a55f5
JacobFeiWang <wf616528291@gmail.com>
2018-08-28 07:30:16 -0400

    Coq Number Practice

--- CoqEx2.v
+++ CoqEx2.v
@@ -10,7 +10,19 @@ Definition pred (n : nat) : nat :=
   | S n' => n'
   end.
 
+Inductive bin_nat : Type :=
+  | b : bin_nat
+  | b0 : bin_nat -> bin_nat
+  | b1 : bin_nat -> bin_nat.
+
 End NatNameSpace.
 
 Check (S (S (S O))).
 (* ===> 3 : nat *)
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+  | O => O
+  | S O => O
+  | S (S n') => n'
+  end.
